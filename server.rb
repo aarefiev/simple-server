11ad18ecd7f265bc3469bdeb95bebaba9b5de11f
Artem Arefiev <a.arefiev@inbox.ru>
2018-06-05 09:03:38 +0400

    Sinatra-reloader

--- server.rb
+++ server.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'json'
 
 set :bind, '0.0.0.0'
 set :port, 4567
+configure :development do
+  enable :reloader
+end
 
 get '/' do
   'OK'
 end
 
-get '/payload' do
-  puts JSON.parse(request.body.read)
+post '/payload' do
+  push = JSON.parse(request.body.read)
+
+  puts push.inspect
 end
